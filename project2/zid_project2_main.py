94c3afdc3411bfe2b68e2d00f1215081e9fe7043
Hoac60 <ANSONLEUNG666@GMAIL.COM>
2024-04-15 12:34:11 +1000

    Initial commit

--- project2/zid_project2_main.py
+++ project2/zid_project2_main.py
@@ -14,7 +14,7 @@
 # Hint: you can utilize modules covered in our lectures, listed above and any others.
 # ----------------------------------------------------------------------------
 
-
+#test
 # ----------------------------------------------------------------------------
 # Part 2: import modules inside the project2 package
 # ----------------------------------------------------------------------------
